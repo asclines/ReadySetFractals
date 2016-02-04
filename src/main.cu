764232cbe2aca2dfe26c6c7c9d2f148efc97b713
asclines <injpk23@gmail.com>
2016-02-04 13:03:40 -0600

    Added code to change fractals ettings in main.cu

--- src/main.cu
+++ src/main.cu
@@ -4,12 +4,25 @@
 
 int main(int argc, const char *argv[]){
 	
-	fractal_generator::GraphSettings graph_settings;
+	fractal_generator::GraphSettings graph_settings;	
+
 	fractal_generator::FractalSettings fractal_settings;
 	fractal_generator::ColorSettings color_settings;
 	fractal_generator::SetPixelsResults *results_ptr;
 
+	graph_settings.radius = 0.1;
+	graph_settings.x_offset = 0.4;
+	graph_settings.y_offset = -0.330;
+
 	fractal_settings.graph_settings = graph_settings;
+	fractal_settings.type = FRACTAL::JULIA;
+	fractal_settings.complex_num = complex(-0.45,0.6);
+	fractal_settings.dimm = 1024;
+	fractal_settings.escape_value = 2;
+	fractal_settings.max_iterations = 1500;
+
+	color_settings.is_bw = false;
+	color_settings.color_offset = 0;
 
 	results_ptr = fractal_generator::GenerateFractal(fractal_settings);//, &results_ptr);
 
